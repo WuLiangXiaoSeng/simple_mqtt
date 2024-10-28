c1bf7dc695e4a2081dceab56268981ba99095dc7
WuLiangXiaoSeng <wuliangxiaosengwzh@163.com>
2024-10-28 18:06:25 +0800

    socket 添加timeout、block配置接口

--- src/network/network.c
+++ src/network/network.c
@@ -1,10 +1,12 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <sys/types.h>
+#include <fcntl.h>
 
 
 #include "network.h"
 #include "log.h"
+#include "mqtt.h"
 
 
 int network_connect(const char *hostname, int port, network_connection_t *conn)
@@ -15,7 +17,7 @@ int network_connect(const char *hostname, int port, network_connection_t *conn)
     conn->socket_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (conn->socket_fd < 0) {
         mqtt_log_error("Failed to create socket");
-        return -1;
+        return MQTT_BROKER_CONNECT_ERROR;
     }
     
     // set server address
@@ -24,16 +26,16 @@ int network_connect(const char *hostname, int port, network_connection_t *conn)
     server_addr.sin_port = htons(port);
     if (inet_pton(AF_INET, hostname, &server_addr.sin_addr) <= 0) {
         mqtt_log_error("Invalid hostname or hostname not support.");
-        return -2;
+        return MQTT_BROKER_HOSTNAME_ERROR;
     }
 
     // connect to server
     if (connect(conn->socket_fd, (struct sockaddr *)&server_addr, sizeof(struct sockaddr_in)) < 0) {
         mqtt_log_error("Failed to connect to server");
-        return -3;
+        return MQTT_BROKER_CONNECT_ERROR;
     }
 
-    return 0;
+    return MQTT_SUCCESS;
 }
 
 
@@ -52,4 +54,35 @@ int network_send(network_connection_t *conn, const void *buffer, size_t len)
 int network_recv(network_connection_t *conn, void *buffer, size_t len)
 {
     return recv(conn->socket_fd, buffer, len, 0);
+}
+
+int network_set_timeout(network_connection_t *conn, int seconds)
+{
+    if (conn == NULL) {
+        return MQTT_INVALID_PARAM;
+    }
+    struct timeval tv;
+    tv.tv_sec = seconds;
+    tv.tv_usec = 0;
+
+    setsockopt(conn->socket_fd, SOL_SOCKET, SO_RCVTIMEO, (const char *)&tv, sizeof(tv));
+
+    return MQTT_SUCCESS;
+}
+
+int network_clean_timeout(network_connection_t *conn) 
+{
+    return network_set_timeout(conn, 0);
+}
+
+int network_unblock(network_connection_t *conn)
+{
+    int flags = fcntl(conn->socket_fd, F_GETFL, 0);
+    return fcntl(conn->socket_fd, F_SETFL, flags | O_NONBLOCK, 0);
+}
+
+int network_block(network_connection_t *conn)
+{
+    int flags = fcntl(conn->socket_fd, F_GETFL, 0);
+    return fcntl(conn->socket_fd, F_SETFL, flags & ~O_NONBLOCK, 0);
 }
--- src/network/network.h
+++ src/network/network.h
@@ -1,6 +1,9 @@
 #ifndef _NETORK_H_
 #define _NETORK_H_
 
+#define MQTT_TCP_PORT 1883
+// #define MQTT_SSL_PORT 8883
+
 typedef struct {
     int socket_fd;
 } network_connection_t;
@@ -10,5 +13,10 @@ int network_connect(const char *hostname, int port, network_connection_t *conn);
 int network_close(network_connection_t *conn);
 int network_send(network_connection_t *conn, const void *buffer, size_t len);
 int network_recv(network_connection_t *conn, void *buffer, size_t len);
+int network_set_timeout(network_connection_t *conn, int seconds);
+int network_clean_timeout(network_connection_t *conn);
+
+int network_unblock(network_connection_t *conn);
+int network_block(network_connection_t *conn);
 
 #endif
